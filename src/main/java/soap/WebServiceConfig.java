636328ba35980ffab30b420b410f2aa3aca4e425
David Grayson <david.grayson@orasi.com>
2018-05-04 09:31:19 -0400

    nope but i'll try try try again

--- src/main/java/soap/WebServiceConfig.java
+++ src/main/java/soap/WebServiceConfig.java
@@ -5,12 +5,16 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.ResourceUtils;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
+
+import java.io.FileNotFoundException;
 
 @EnableWs
 @Configuration
@@ -37,8 +41,8 @@ public class WebServiceConfig extends WsConfigurerAdapter {
 	}
 
 	@Bean(name = "operationsSchema")
-	public XsdSchema operationSchema(){
-		return new SimpleXsdSchema(new ClassPathResource("xsd/operations.xsd"));
+	public XsdSchema operationSchema() throws FileNotFoundException {
+		return new SimpleXsdSchema((Resource) ResourceUtils.getFile("target/classes/xsd/operations.xsd"));
 	}
 
 	@Bean(name = "data_elements")
