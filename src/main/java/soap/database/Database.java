e67d6aeaf921663eebf0f27b0d91545d9724950c
David Grayson <david.grayson@orasi.com>
2018-05-03 20:29:05 -0400

    changes to db connection mehods

--- src/main/java/soap/database/Database.java
+++ src/main/java/soap/database/Database.java
@@ -5,54 +5,42 @@
 
 package soap.database;
 
+import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import soap.database.entity.ActorEntity;
-import soap.database.entity.FilmEntity;
-import soap.database.entity.LanguageEntity;
 
-import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
-@EntityScan(basePackageClasses = {ActorEntity.class,FilmEntity.class,LanguageEntity.class})
+@EntityScan(basePackages = "soap.database.entity")
 public class Database {
-	private static final String dbUrl = "postgres://mnjtlhsosvnbhb:4d6195c6d64ca5e53ad6844e113aef816dc8b8f39125ae02f3e67d178b1545d3@ec2-54-243-137-182.compute-1.amazonaws.com:5432/d34f317612ph8k";
-	public Database() {}
 
 	@Bean
-	public Properties additionalProperties() {
-		Properties properties = new Properties();
-		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.ProgressDialect");
-		return properties;
+	public BasicDataSource dataSource() throws URISyntaxException, SQLException {
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		String dbUrl = "jdbc:postgresql://"+dbUri.getHost()+":"+dbUri.getPort()+dbUri.getPath()+"?sslmode=required";
+
+		BasicDataSource basicDataSource = new BasicDataSource();
+		basicDataSource.setUrl(dbUrl);
+		basicDataSource.setUsername(username);
+		basicDataSource.setPassword(password);
+
+		return basicDataSource;
 	}
 
 	@Bean
-	public DataSource dataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("org.postgresql.Driver");
-		dataSource.setUrl(dbUrl);
-		dataSource.setUsername("sakila");
-		dataSource.setPassword("sakila");
-		return dataSource;
-	}
-
-	@Bean
-	public Connection getConnection(){
-		try {
-			Class.forName("org.postgresql.Driver").newInstance();
-			java.sql.Connection connection = DriverManager.getConnection(dbUrl,"sakila","sakila");
-			return (Connection) connection;
-		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
-			e.printStackTrace();
-		}
-		return null;
+	public Connection getConnection() throws SQLException {
+		String dbUrl = System.getenv("JDBC_DATABASE_URL");
+		return DriverManager.getConnection(dbUrl);
 	}
 }
