50b81e207385156af694821098a284f91f7cf9dd
David Grayson <david.grayson@orasi.com>
2018-05-07 20:25:09 -0400

    removed autowired film and actor dao from filmactordao

--- src/main/java/soap/database/dao/FilmActorDao.java
+++ src/main/java/soap/database/dao/FilmActorDao.java
@@ -1,6 +1,5 @@
 package soap.database.dao;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import soap.database.entity.FilmActorEntity;
 import soap.generated.Actor;
@@ -14,18 +13,16 @@ import java.util.List;
 public class FilmActorDao {
 	@PersistenceContext
 	private EntityManager em;
-	@Autowired private ActorDao actorDao;
-	@Autowired private FilmDao filmDao;
 	private String baseQuery = "SELECT fa from film_actor fa ";
 
 	List<Actor> getActors(long film_id) {
-//		ActorDao actorDao = new ActorDao();
+		ActorDao actorDao = new ActorDao();
 		System.out.println("film actor dao get actors");
 		return actorDao.getAllActors(this.em.createQuery(baseQuery+"WHERE film_actor.film_id = "+film_id,FilmActorEntity.class).getResultList());
 	}
 
 	List<Film> getFilms(long actor_id) {
-//		FilmDao filmDao = new FilmDao();
+		FilmDao filmDao = new FilmDao();
 		return filmDao.getAllFilms(this.em.createQuery(baseQuery+"WHERE fa.actor_id = "+actor_id, FilmActorEntity.class).getResultList());
 	}
 }
