45d1496380edd8732207fb03b55620bbfd7eb46a
David Grayson <david.grayson@orasi.com>
2018-05-07 20:39:21 -0400

    used a typedQuery instead of sql string

--- src/main/java/soap/database/dao/FilmCategoryDao.java
+++ src/main/java/soap/database/dao/FilmCategoryDao.java
@@ -6,6 +6,11 @@ import soap.database.entity.FilmCategoryEntity;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.persistence.criteria.Selection;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class FilmCategoryDao {
@@ -15,7 +20,17 @@ public class FilmCategoryDao {
 	private String baseQuery = "SELECT fc FROM sakila.film_category ";
 
     public String getById(long film_id) {
-        FilmCategoryEntity fce = this.em.createQuery(baseQuery+"WHERE fc.film_id = :film_id",FilmCategoryEntity.class).setParameter("film_id",film_id).getSingleResult();
+        CriteriaQuery<FilmCategoryEntity> query = em.getCriteriaBuilder().createQuery(FilmCategoryEntity.class);
+        Root<FilmCategoryEntity> filmCategoryEntityRoot = query.from(FilmCategoryEntity.class);
+
+        List<Selection<?>> selections = new ArrayList<>();
+        selections.add(filmCategoryEntityRoot.get("film_id"));
+        selections.add(filmCategoryEntityRoot.get("category_id"));
+
+        query.multiselect(selections);
+        query.where(em.getCriteriaBuilder().equal(filmCategoryEntityRoot.get("film_id"),film_id));
+
+        FilmCategoryEntity fce = this.em.createQuery(query).getSingleResult();
         return categoryDao.getNameById(fce.getCategory_id());
 	}
 }
