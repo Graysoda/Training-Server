1fc2663123a7df72e1cd19d260a6b18be0c785a7
David Grayson <david.grayson@orasi.com>
2018-05-09 16:45:08 -0400

    added Lazy annotation

--- src/main/java/soap/service/InventoryServiceImpl.java
+++ src/main/java/soap/service/InventoryServiceImpl.java
@@ -1,6 +1,7 @@
 package soap.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import soap.database.dao.InventoryDao;
@@ -13,7 +14,7 @@ import java.util.List;
 
 @Service
 public class InventoryServiceImpl implements InventoryService{
-	@Autowired private InventoryDao inventoryDao;
+	@Autowired @Lazy private InventoryDao inventoryDao;
 
 //	@Autowired
 //	public void setInventoryDao(@Lazy InventoryDao inventoryDao) {
