6d0d18ab1c53393ca017a1921a206399bbb8be92
David Grayson <david.grayson@orasi.com>
2018-05-09 14:41:18 -0400

    added Lazy annotation to EntityManager

--- src/main/java/soap/database/dao/FilmDao.java
+++ src/main/java/soap/database/dao/FilmDao.java
@@ -1,6 +1,7 @@
 package soap.database.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Repository;
 import soap.database.Database;
 import soap.database.entity.FilmEntity;
@@ -20,7 +21,7 @@ import java.util.List;
 
 @Repository
 public class FilmDao extends Database{
-	@PersistenceContext
+	@PersistenceContext @Lazy
 	private EntityManager em;
 	@Autowired private LanguageDao languageDao;
 	@Autowired private FilmCategoryDao filmCategoryDao;
