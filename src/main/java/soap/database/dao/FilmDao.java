bf258399fc70e0db6d5113aced9393b93d489a58
David Grayson <david.grayson@orasi.com>
2018-05-09 16:56:46 -0400

    removed mpaa_rating valueOf expression

--- src/main/java/soap/database/dao/FilmDao.java
+++ src/main/java/soap/database/dao/FilmDao.java
@@ -72,7 +72,7 @@ public class FilmDao extends Database{
 
 		query.distinct(true);
 		query.multiselect(makeSelections(query));
-		query.where(this.em.getCriteriaBuilder().equal(ratingField.as(String.class),FilmEntity.mpaa_rating.valueOf(rating)));
+		query.where(this.em.getCriteriaBuilder().equal(ratingField.as(String.class),rating));
 
 		return convertListToGenerated(this.em.createQuery(query).setMaxResults(50).getResultList());
 	}
