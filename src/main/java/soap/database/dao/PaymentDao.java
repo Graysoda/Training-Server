4e76d84da7448a7b3f1519d3dbe6606b03e39377
David Grayson <david.grayson@orasi.com>
2018-05-09 15:48:27 -0400

    added Lazy annotation to EntityManager

--- src/main/java/soap/database/dao/PaymentDao.java
+++ src/main/java/soap/database/dao/PaymentDao.java
@@ -1,5 +1,6 @@
 package soap.database.dao;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Repository;
 import soap.database.Database;
 import soap.database.entity.PaymentEntity;
@@ -19,7 +20,7 @@ import java.util.List;
 
 @Repository
 public class PaymentDao extends Database {
-	@PersistenceContext
+	@PersistenceContext @Lazy
 	private EntityManager em;
 
 //	@Autowired
