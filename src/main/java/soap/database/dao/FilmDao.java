6303eb5b1e657e4708a945540dd1e18cd6b0623e
David Grayson <david.grayson@orasi.com>
2018-05-10 08:52:42 -0400

    dfg

--- src/main/java/soap/database/dao/FilmDao.java
+++ src/main/java/soap/database/dao/FilmDao.java
@@ -93,7 +93,7 @@ public class FilmDao extends Database{
 
 		query.distinct(true);
 		query.multiselect(makeSelections(query));
-		query.where(this.em.getCriteriaBuilder().equal(from.get("title"),title.toUpperCase()));
+		query.where(this.em.getCriteriaBuilder().equal(from.get("title").as(String.class),title.toUpperCase()));
 
 		return convertListToGenerated(this.em.createQuery(query).setMaxResults(50).getResultList());
 	}
